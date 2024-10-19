8425434e6c8a1fec6e0f08b2d5ab0d29be20ae2b
MedrekIT <dan.creez@o2.pl>
2024-10-19 18:03:58 +0200

    Changes in project - 6:03 PM 19.10.2024

--- salesImprovement/main.py
+++ salesImprovement/main.py
@@ -3,24 +3,44 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from sklearn.preprocessing import LabelEncoder
 from keras.layers import GRU
 
 salesData = pd.read_csv("Sales Data.csv", index_col=0, sep=",")
 dataName = list(salesData.columns)
 data = salesData.values
 
-
 for row in data:
     row[4] = datetime.strptime(row[4], '%Y-%m-%d %H:%M:%S')
+    
 
 salesByDate = pd.DataFrame(data, columns=dataName)
+salesByDate['Order Date'] = pd.to_datetime(salesByDate['Order Date'])
 salesByDate = salesByDate[salesByDate['Order Date'].dt.year == 2019]
+
 salesByDate = salesByDate.groupby(salesByDate['Order Date'].dt.date)['Sales'].sum()
+
 salesByDate.plot()
 plt.show()
-test = salesByDate.to_frame()
 
-print(list(test.values))
+data = pd.DataFrame(data, columns=dataName)
+data['Year'] = data['Order Date'].dt.year
+data['Month'] = data['Order Date'].dt.month
+data['Day'] = data['Order Date'].dt.day
+data['DayOfWeek'] = data['Order Date'].dt.dayofweek
+data['Hour'] = data['Order Date'].dt.hour
+
+label_encoder = LabelEncoder()
+data['Product Code'] = label_encoder.fit_transform(data['Product'])
+
+data = pd.get_dummies(data, columns=['City'], drop_first=True)
+
+print(data)
+
+salesByDate = salesByDate.to_frame().reset_index()
+salesSumData = salesByDate.values
+salesSumCols = list(salesByDate.columns)
+
 
 #plt.plot(x, y)
 #plt.xlabel('Date')
